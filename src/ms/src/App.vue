486cad7723140aff3e7e071ae14a24e5782870b0
zhaoyue0218 <2289956458@qq.com>
2019-05-18 10:51:38 +0800

    添加

---
+++ src/ms/src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld/> -->
+    <header>
+      <ul>
+        <li><i class="icon iconfont icon-zuojiantou-01"></i></li>
+        <li>面试列表</li>
+        <li>
+        <span><i class="icon iconfont icon-gengduo"></i></span>
+        <span><i class="icon iconfont icon-gengduo"></i></span>
+        </li>
+      </ul>
+      <ol>
+        <li v-for="(item,index) in data" :key="index" :class="{active:index==ind}" v-on:click="activ(index)">{{item.lis}}</li>
+      </ol>
+    </header>
+    <mysection :data="data[ind].inners" :ind="ind" >
+    </mysection>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Mysection from './components/mysection'
+import './fonts/iconfont.css'
+import data from './mock/data.json'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Mysection
+  },
+    data(){
+        return {
+            data:data,
+            ind:0
+        }
+    },
+    methods:{
+      activ(index){
+        console.log(index)
+        this.ind=index
+      }
+    }
+}
+</script>
+
+<style>
+*{
+  list-style: none;
+  margin:0;
+  padding:0;
+}
+body,html{
+  width: 100%;
+  height: 100%;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+}
+.active{
+  color: skyblue;
+  border-bottom: 1px solid skyblue;
+}
+header{
+  width: 100%;
+  height:80px;
+
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+header ul{
+ width: 100%;
+  height: 40px;
+  box-sizing: border-box;
+  padding: 0 10px;
+ display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #ccc;
+}
+header ol{
+  width: 100%;
+  height: 40px;
+  box-sizing: border-box;
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+header ol li{
+  height: 100%;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #fff;
+}
+</style>
---
+++ src/ms/src/components/mysection.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="section">
+<!-- {{ind}} -->
+<ul  v-for="(item,index) in data" :key="index" >
+    <li><h4>{{item.nav}}</h4><span>{{item.fangqi}}</span></li>
+    <li>{{item.dizhi}}</li>
+    <li>{{item.shijian}}<span >{{item.tixing}}</span></li>
+    <li >{{item.nul}}</li>
+</ul>
+    </div>
+</template>
+<script>
+export default {
+    props:["data","ind"],
+    components:{
+
+    },
+    data(){
+        return {
+           col:'#skyblue'
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+.section{
+    width: 100%;
+    flex-grow:1;
+}
+.section ul{
+ width: 100%;
+  /* height: 40px; */
+  box-sizing: border-box;
+  padding: 10px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border-top:10px solid #eee;
+}
+.section ul li{
+ width: 100%;
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+ margin: 3px;
+}
+.section ul li:nth-child(1) h4{
+    font-size: 20px;
+}
+.section ul li:nth-child(1) span{
+    font-size: 14px;
+    background: rgb(253, 227, 227);
+    color: rgb(236, 113, 113);
+    padding: 5px 5px;
+}
+.section ul li:nth-child(2){
+    font-size: 17px;
+    color: #ccc;
+}
+.section ul li:nth-child(3){
+    /* font-size: 17px; */
+    color: rgb(122, 120, 120);
+}
+.section ul li:nth-child(3) span{
+    font-size: 14px;
+    background: rgb(238, 237, 237);
+    color: rgb(189, 184, 184);
+    padding: 5px 5px;
+}
+</style>
